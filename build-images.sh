6f67325ddd800d0527b16019ed5f704d97b593a4
kemboi22 <kemboielvis22@gmail.com>
2024-02-08 03:17:14 +0300

    Added Neccessary services for erp-next

--- build-images.sh
+++ build-images.sh
@@ -11,24 +11,24 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/nethserver}"
+repobase="${REPOBASE:-ghcr.io/kemboielvis}"
 # Configure the image name
-reponame="kickstart"
+reponame="erpnext"
 
 # Create a new empty container image
 container=$(buildah from scratch)
 
-# Reuse existing nodebuilder-kickstart container, to speed up builds
-if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-kickstart; then
+# Reuse existing nodebuilder-erpnext container, to speed up builds
+if ! buildah containers --format "{{.ContainerName}}" | grep -q nodebuilder-erpnext; then
     echo "Pulling NodeJS runtime..."
-    buildah from --name nodebuilder-kickstart -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
+    buildah from --name nodebuilder-erpnext -v "${PWD}:/usr/src:Z" docker.io/library/node:lts
 fi
 
 echo "Build static UI files with node..."
 buildah run \
     --workingdir=/usr/src/ui \
     --env="NODE_OPTIONS=--openssl-legacy-provider" \
-    nodebuilder-kickstart \
+    nodebuilder-erpnext \
     sh -c "yarn install && yarn build"
 
 # Add imageroot directory to the container image
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/nginx:stable-alpine3.17" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/frappe/erpnext:v15.12.2 docker.io/library/redis:6.2-alpine" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
