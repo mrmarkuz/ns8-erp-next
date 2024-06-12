017570701c239ddff41126dabf49bab3e2826281
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-06-12 22:50:15 +0200

    Fix redis image

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/frappe/erpnext:${app_version} docker.io/library/redis:6.2-alpine" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/frappe/erpnext:${app_version} docker.io/redis:6.2-alpine" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
